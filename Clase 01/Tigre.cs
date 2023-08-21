f50163ff6f402c55f7c6e65b3cda089e4e90937f
MauricioAlmada <110863446+AlmadaMauricio@users.noreply.github.com>
2023-08-21 19:26:06 -0300

    Repaso de poo en C#
    Herencia, listas, sobreescritura de métodos, interfaces, asociación.

---
+++ Clase 01/Tigre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo6_Herencia
+{
+    internal class Tigre : AnimalSalvaje
+    {
+    }
+}
