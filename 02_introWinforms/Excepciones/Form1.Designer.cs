356afa557c60de881a35b480f8085967e70ac546
mauriciogaston.almada <mauriciogaston.almada@gmail.com>
2023-09-08 22:17:49 -0300

    Subiendo archivos con excepciones

---
+++ 02_introWinforms/Excepciones/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace manejoExcepciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtUno = new System.Windows.Forms.TextBox();
+            this.txtDos = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(182, 185);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtUno
+            // 
+            this.txtUno.Location = new System.Drawing.Point(161, 91);
+            this.txtUno.Name = "txtUno";
+            this.txtUno.Size = new System.Drawing.Size(114, 20);
+            this.txtUno.TabIndex = 1;
+            // 
+            // txtDos
+            // 
+            this.txtDos.Location = new System.Drawing.Point(161, 143);
+            this.txtDos.Name = "txtDos";
+            this.txtDos.Size = new System.Drawing.Size(114, 20);
+            this.txtDos.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(217, 241);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(13, 13);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 402);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtDos);
+            this.Controls.Add(this.txtUno);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtUno;
+        private System.Windows.Forms.TextBox txtDos;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ 02_introWinforms/Excepciones/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manejoExcepciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int resultado;
+            try
+            {
+                resultado = calcular();
+                lblResultado.Text = "= " + resultado;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error inesperado, contactar a tu dev.");
+            }
+            finally
+            {
+
+            }
+        }
+
+        private int calcular()
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(txtUno.Text);
+                b = int.Parse(txtDos.Text);
+                r = a / b;
+                return r;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
